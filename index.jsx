9229b5b5eebe86e3437cb7552f925d956f6b83ce
FolagbadeDan <contactyoumaximize@gmail.com>
2023-03-12 13:54:50 +0100

    changed qoutes

--- index.jsx
+++ index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from './pages/Vans';
+import Vans from "./pages/Vans";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         </nav>
       </header>
       <Routes>
-      <Route path="/vans" element={<Vans />} />
+      <Route path="/vans" element={<Vans/>} />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
